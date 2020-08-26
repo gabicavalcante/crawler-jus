52bc6a9a625ec9effdb2e18dc026c106d19532af
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-08-25 22:54:24 -0300

    fix settings to rabbit

--- crawler_jus/ext/broker.py
+++ crawler_jus/ext/broker.py
@@ -2,8 +2,10 @@ import dramatiq
 
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from dynaconf import settings
+from loguru import logger
 
 
-def create_app(app):
+def init_app(app):
+    logger.debug(f"connecting to {settings.RABBIT_HOST}")
     rabbitmq_broker = RabbitmqBroker(host=settings.RABBIT_HOST)
     dramatiq.set_broker(rabbitmq_broker)
