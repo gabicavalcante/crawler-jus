1bac664ce6b41d1ecbd4aeaa166e07269706e056
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-08-25 23:36:59 -0300

    remove test with rabbit

--- crawler_jus/app.py
+++ crawler_jus/app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
-import dramatiq
-
-from dramatiq.brokers.rabbitmq import RabbitmqBroker
 
 
 def create_app(**config) -> Flask:
@@ -11,9 +8,6 @@ def create_app(**config) -> Flask:
     :return: A Flask object
     """
     app = Flask(__name__)
-
-    rabbitmq_broker = RabbitmqBroker(host="0.0.0.0")
-    dramatiq.set_broker(rabbitmq_broker)
 
     FlaskDynaconf(
         app, settings_files=["settings.toml"], extensions_list="EXTENSIONS", **config
