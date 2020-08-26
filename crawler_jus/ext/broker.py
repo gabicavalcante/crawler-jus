b6fcd17c8ddcdf346b6aefd3514b183a7cfeca9a
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-08-25 22:50:24 -0300

    [QA] black

--- crawler_jus/ext/broker.py
+++ crawler_jus/ext/broker.py
@@ -3,6 +3,7 @@ import dramatiq
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from dynaconf import settings
 
+
 def create_app(app):
     rabbitmq_broker = RabbitmqBroker(host=settings.RABBIT_HOST)
     dramatiq.set_broker(rabbitmq_broker)
