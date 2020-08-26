9d8472261bfd9ca066ca9d58b640c097db6927ea
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-08-25 22:44:51 -0300

    add variable REDIS_PORT

---
+++ crawler_jus/ext/broker.py
@@ -0,0 +1,8 @@
+import dramatiq
+
+from dramatiq.brokers.rabbitmq import RabbitmqBroker
+from dynaconf import settings
+
+def create_app(app):
+    rabbitmq_broker = RabbitmqBroker(host=settings.RABBIT_HOST)
+    dramatiq.set_broker(rabbitmq_broker)
