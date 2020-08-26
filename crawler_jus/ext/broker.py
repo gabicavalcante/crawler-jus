5caad93407629d204bbf8186854cf017ddd39d20
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-08-25 22:58:25 -0300

    test host to CI

--- crawler_jus/ext/broker.py
+++ crawler_jus/ext/broker.py
@@ -7,5 +7,5 @@ from loguru import logger
 
 def init_app(app):
     logger.debug(f"connecting to {settings.RABBIT_HOST}")
-    rabbitmq_broker = RabbitmqBroker(host=settings.RABBIT_HOST)
+    rabbitmq_broker = RabbitmqBroker(host="0.0.0.0")
     dramatiq.set_broker(rabbitmq_broker)
